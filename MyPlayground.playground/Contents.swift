2efa1085156b079446a7693aedbd071887bfca11
AutoAvaliar <cs.couto@outlook.com>
2017-07-14 11:38:17 -0300

    initial

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//declarative
+let numbers = [9, 29, 19, 79]
+
+let tripledValues = numbers.map{number in number * 3}
+print(tripledValues)
+
