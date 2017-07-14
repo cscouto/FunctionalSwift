a3cfdb35a7ab218b8f43af2a83b947828a0c9096
AutoAvaliar <cs.couto@outlook.com>
2017-07-14 12:00:17 -0300

    page 64

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,8 +3,12 @@
 import UIKit
 
 //declarative
-let numbers = [9, 29, 19, 79]
+let numbers = [1, 2, 3, 4]
 
 let tripledValues = numbers.map{number in number * 3}
 print(tripledValues)
+
+//lazy - only evaluate the first data in the array
+let number = numbers.lazy.map{$0 * 3}.first!
+print(number)
 
