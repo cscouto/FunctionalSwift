314936831a5f0a289f90afc51f9c5c945080cac9
AutoAvaliar <cs.couto@outlook.com>
2017-07-17 10:24:39 -0300

    closures

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -34,3 +34,24 @@ typealias UnsignedInteger = UInt32
 //stride
 let fourToTwo = Array(stride(from: 4, to: 1, by: -1)) // [4, 3, 2]
 let fourToOne = Array(stride(from:4, through: 1, by: -1)) // [4, 3, 2, 1]
+
+//closures
+// a closure that has no parameters and return a String
+var hello: () -> (String) = {
+    return "Hello!"
+}
+
+hello() // Hello!
+
+// a closure that take one Int and return an Int
+var double: (Int) -> (Int) = { x in
+    return 2 * x
+}
+
+double(2) // 4
+
+// you can pass closures in your code, for example to other variables
+var alsoDouble = double
+
+alsoDouble(3) // 6
+
