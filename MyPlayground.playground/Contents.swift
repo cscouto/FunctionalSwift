800ca51a3a37aa1ed23075853de9f3f7ccd44235
AutoAvaliar <cs.couto@outlook.com>
2017-07-17 14:02:23 -0300

    page 199

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -55,3 +55,19 @@ var alsoDouble = double
 
 alsoDouble(3) // 6
 
+//functions
+func extractElements(_ content: String) -> [String] {
+    return content.characters.split(separator: ",").map { String($0) }
+}
+
+//let elements = extractElements(content)
+
+func formatWithCurrency(content: [String]) -> [String] {
+    return content.map {"\($0)$"}
+}
+
+//let formattedElements = formatWithCurrency(content: elements)
+let composedFunction = { data in
+    formatWithCurrency(content: extractElements(data))
+}
+
